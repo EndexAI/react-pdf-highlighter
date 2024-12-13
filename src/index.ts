17614da74990c0429343a6cfb1c944c51608b46a
Kelvin Zhang <me@kelvinzhang.ca>
2024-12-13 17:27:07 -0500

    upgrade to react 19

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,31 @@ export { Popup } from "./components/Popup";
 export { AreaHighlight } from "./components/AreaHighlight";
 export { PdfLoader } from "./components/PdfLoader";
 
-export * from "./types";
+export { scaledToViewport, viewportToScaled } from "./lib/coordinates";
+export { default as getBoundingRect } from "./lib/get-bounding-rect";
+export { default as getClientRects } from "./lib/get-client-rects";
+export { default as getAreaAsPng } from "./lib/get-area-as-png";
+export {
+  findOrCreateContainerLayer,
+  getPageFromElement,
+  getPagesFromRange,
+  getWindow,
+  isHTMLElement,
+} from "./lib/pdfjs-dom";
+
+export type {
+  LTWH,
+  LTWHP,
+  Scaled,
+  Position,
+  ScaledPosition,
+  Content,
+  HighlightContent,
+  Comment,
+  HighlightComment,
+  NewHighlight,
+  IHighlight,
+  ViewportHighlight,
+  Viewport,
+  Page,
+} from "./types";
